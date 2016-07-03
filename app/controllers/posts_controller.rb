da942cf08885be4f8b95194521ecf9c2f8a2371a
yamotech <nothings.2c9@gmail.com>
2016-07-03 17:52:06 +0900

    検索機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,9 @@ class PostsController < ApplicationController
   before_action :set_post, only: [ :show, :edit, :update, :destroy ]
 
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @q = Post.order(created_at: :desc).ransack(params[:q])
+    @posts = @q.result(distinct: true)
+    @new_posts = Post.order(created_at: :desc).limit(5)
   end
 
   def show
