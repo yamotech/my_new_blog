f109f579bc31485bb5af38f441b4755809616841
yamotech <nothings.2c9@gmail.com>
2016-07-03 15:23:01 +0900

    edit機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,9 +17,13 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to "/posts/#{@post.id}"
   end
 
   def destroy
