2c887d29315c02eb8242530424fa110e55d5fa90
yamotech <nothings.2c9@gmail.com>
2016-07-03 14:59:17 +0900

    PostsControllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/edit'
+
+  get 'posts/update'
+
+  get 'posts/destroy'
+
   devise_for :authors
   root 'posts#index'
 
